2b141559732f1b5848352b7f168f457df3e8a216
Andrei Kniazev <andrei.kniazev@yorlabs.com>
2016-05-27 08:40:55 -0700

    OpenGL testing added

--- main.cpp
+++ main.cpp
@@ -1,10 +1,18 @@
 #include "mygl.h"
 #include <QApplication>
+#include <QtOpenGL/QGLFormat>
+#include <iostream>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     myGL w;
+
+    if(!QGLFormat::hasOpenGL()) {
+        std::cerr << "This system has no OpenGL support" << std::endl;
+        return -1;
+    }
+
     w.show();
 
     return a.exec();
